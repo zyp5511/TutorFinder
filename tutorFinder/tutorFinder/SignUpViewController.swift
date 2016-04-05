c06809780951505ea46652abf42429bccd65a70a
hank1106 <hankzhang06@gmail.com>
2016-04-04 20:18:49 -0500

    signup

---
+++ tutorFinder/tutorFinder/SignUpViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SignUpViewController.swift
+//  tutorFinder
+//
+//  Created by Zihan Zhang on 4/4/16.
+//  Copyright © 2016 ZhangYipeng. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var reenter: UITextField!
+    
+    @IBOutlet weak var sex: UIPickerView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signupButton(sender: AnyObject) {
+        //save the data to the database
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
