36db771dc9845ef41788b7415f30314cea080526
jiwei1 <bibijijiweihch@gmail.com>
2016-04-17 15:40:27 -0500

    LoopBack Setup

---
+++ tutorFinder/Pods/Target Support Files/Pods-tutorFinderUITests/Pods-tutorFinderUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_tutorFinderUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_tutorFinderUITestsVersionString[];
+
--- tutorFinder/tutorFinder/InboxTableViewController.swift
+++ tutorFinder/tutorFinder/InboxTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LoopBack
 
 class InboxTableViewController: UITableViewController {
 
